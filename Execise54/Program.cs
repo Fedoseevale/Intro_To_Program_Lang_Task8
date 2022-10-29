93d225c04d3c006c17efc6b08ed9191eea41ad07
Александр <fedoseevale@gmail.com>
2022-10-29 17:00:57 +0400

    Execise54 done

--- Execise54/Program.cs
+++ Execise54/Program.cs
@@ -8,3 +8,59 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // 0 - rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 1 - colomns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} | ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void MatrixStreamlineFromMaxToMin(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int position = j;
+            for (int k = 0; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, k] < matrix[i, position])
+                {
+                    position = k;
+                }
+                int temporary = matrix[i, j];
+                matrix[i, j] = matrix[i, position];
+                matrix[i, position] = temporary;
+            }
+        }
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(5, 5, 100, 1000);
+PrintMatrix(array2D);
+Console.WriteLine();
+MatrixStreamlineFromMaxToMin(array2D);
+PrintMatrix(array2D);
